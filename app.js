9cdc2447ba393c1178a26e50f632ddaf60f5ca0c
Carvalho0331 <96897549+Carvalho0331@users.noreply.github.com>
2026-01-05 16:11:25 +0200

    Update app.js

--- app.js
+++ app.js
@@ -107,14 +107,8 @@ app.get("/categorias/postagens/:slug",(req,res)=>{
 
 
 
-
-
-
-
-
-
-
-const port = 3000
+const port = process.env.PORT || 3000
 app.listen(port, ()=>{
-    console.log("Server running at http//localhost:"+port)
+    console.log("Servidor Rodando")
+
 })
